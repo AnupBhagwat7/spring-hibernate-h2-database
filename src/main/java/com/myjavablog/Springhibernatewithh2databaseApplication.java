a997bbe81950ba598c6905399c8fe6c3facf3f24
anupb <anup.bhagwat7@gmail.com>
2018-12-30 08:28:20 +0530

    Spring boot data JPA with H2 databse

---
+++ src/main/java/com/myjavablog/Springhibernatewithh2databaseApplication.java
@@ -0,0 +1,14 @@
+package com.myjavablog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springhibernatewithh2databaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springhibernatewithh2databaseApplication.class, args);
+	}
+
+}
+
